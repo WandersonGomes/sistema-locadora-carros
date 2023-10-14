f391f1fc410b03d148d02b1997551a5c269b9adf
WandersonGomes <dersom100@gmail.com>
2023-10-14 15:53:49 -0300

    FavIcon e Express

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const port = 3000;
@@ -7,11 +8,12 @@ const port = 3000;
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
 app.listen(port, () => {
-    console.log(`Servidor funcionando na porta ${port}.`);
+    console.log(`Servidor funcionando em https://localhost:${port}.`);
 });
