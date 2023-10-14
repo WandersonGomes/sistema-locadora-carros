2be0c302f950d3b97300ec473831f5af00d45697
WandersonGomes <dersom100@gmail.com>
2023-10-14 15:39:49 -0300

    Iniciando Sequelize, Bootstrap e Handlebars

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+
+const app = express();
+const port = 3000;
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor funcionando na porta ${port}.`);
+});
