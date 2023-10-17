891c68d2c9925fe6c2152c4c980ebda131c28e01
isaac <isaac.sa.silva08@aluno.ifce.edu.br>
2023-10-17 16:10:17 -0300

    adicionado tela login e icone do carro

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const { engine } = require('express-handlebars');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-const autenticacaoJWT = require('./middlewares/autenticacaoJWT');
+//const autenticacaoJWT = require('./middlewares/autenticacaoJWT');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
@@ -14,8 +14,14 @@ app.set('views', './views');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
+//principal
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+//login
+app.get('/login', (req, res) => {
+    res.render('login');
 });
 
 app.get('/')
