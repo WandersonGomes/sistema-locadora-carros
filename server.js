8f8374a979f3b71a30968ab5fdcb1f49e0f2ca81
isaac <isaac.sa.silva08@aluno.ifce.edu.br>
2023-10-25 15:01:06 -0300

    linkando as telas e adicionando as rotas para telas

--- server.js
+++ server.js
@@ -19,14 +19,39 @@ app.set('views', './views');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
-//principal
+//login
 app.get('/', (req, res) => {
     res.render('login');
 });
 
-//login
+//post do login -> vai para home
+app.post('/', (req, res) =>{
+    res.render('home')
+})
+
+//home
 app.get('/home', (req, res) => {
     res.render('home');
+});
+
+//aluguel
+app.get('/aluguel', (req, res) => {
+    res.render('aluguel');
+});
+
+//clientes
+app.get('/clientes', (req, res) => {
+    res.render('clientes');
+});
+
+//sobre
+app.get('/sobre', (req, res) => {
+    res.render('sobre');
+});
+
+//veiculos
+app.get('/veiculos', (req, res) => {
+    res.render('veiculos');
 });
 
 app.listen(port, () => {
